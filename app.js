a970c99a4a0b11b095686ca92b0b386c2bbbe707
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-12-18 12:05:14 +0100

    feat: counter post

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const posts = require('./data/posts')
 const app = express();
 const port = 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
  res.send('Server del mio blog')
 })
+
+app.get('/bacheca', (req, res)=>{
+  
+  const post = {
+    conteggio : posts.length,
+    posts
+  }
+  res.json(post)
+})
+
 app.listen(port, ()=>{
   console.log('Sono in ascolto')
 })
---
+++ data/posts.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Il ciambellone è un classico della cucina italiana, ideale per la colazione o una merenda sana e golosa. Con una consistenza soffice e un sapore delicato, questa ricetta è perfetta anche per essere personalizzata con gocce di cioccolato o frutta secca.",
+    immagine: "/public/images/ciambellone.jpeg",
+    tags: ["ciambellone", "colazione", "merenda", "dolci", "ricetta italiana"]
+  },
+  {
+    titolo: "Cracker alla barbabietola",
+    contenuto: "I cracker alla barbabietola sono una variante sana e saporita dei classici cracker salati. Grazie al colore rosso vivo e al gusto leggermente terroso della barbabietola, questi cracker sono perfetti come snack leggero o come accompagnamento per un antipasto.",
+    immagine: "/public/images/cracker_barbabietola.jpeg",
+    tags: ["cracker", "barbabietola", "snack", "vegano", "ricetta salutare"]
+  },
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "Il pane fritto dolce è una ricetta semplice ma irresistibile. Si tratta di fette di pane arricchite con zucchero e spezie, fritte fino a diventare croccanti e dorate. Perfetto per un dessert veloce o per una merenda golosa.",
+    immagine: "/public/images/pane_fritto_dolce.jpeg",
+    tags: ["pane fritto", "dolce", "merenda", "ricetta facile", "dessert"]
+  },
+  {
+    titolo: "Pasta alla barbabietola",
+    contenuto: "La pasta alla barbabietola è una ricetta colorata e ricca di nutrienti. La barbabietola dona al piatto una tonalità rosata e un sapore delicato, rendendo questa pasta perfetta per un pranzo sano e gustoso. Provala con un sugo semplice a base di panna e formaggio.",
+    immagine: "/public/images/pasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola", "piatto vegetariano", "ricetta colorata", "cucina sana"]
+  },
+  {
+    titolo: "Torta paesana",
+    contenuto: "La torta paesana è un dolce rustico tipico della tradizione contadina. Con ingredienti semplici come il pane raffermo, il latte e il cacao, questa torta è perfetta per riutilizzare il pane avanzato e preparare un dolce goloso e profumato.",
+    immagine: "/public/images/torta_paesana.jpeg",
+    tags: ["torta paesana", "dolce rustico", "pane raffermo", "cucina tradizionale", "ricetta economica"]
+  }
+];
+
+module.exports = posts;
